462bf46b222e9918d4e9fcbd0e73be408917d920
Igor Anić <igor.anic@gmail.com>
2023-03-27 17:31:56 +0200

    some build changes

--- users/ianic/home-manager.nix
+++ users/ianic/home-manager.nix
@@ -8,7 +8,6 @@
     ./git.nix
     ./desktop.nix
   ];
-  
   # Home-manager 22.11 requires this be set. We never set it so we have
   # to use the old state version.
   home.stateVersion = "18.09";
